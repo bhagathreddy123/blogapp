38b62be22e5b055680c70ad1fd459a277a304c81
bhagathreddy123 <katta_reddy04@yahoo.com>
2023-08-03 11:12:49 -0700

    Rails 7 Ruby 3.2.2 basic blog application

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,17 @@
+class ArticlesController < ApplicationController
+  def index
+     @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+end
